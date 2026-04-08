36539df1aaaedc5b2c2b562b3fcefd087b128cbc
Chris Haumesser <5400416-wryfi@users.noreply.gitlab.com>
2026-04-08 14:40:43 -0700

    improve landing page styling

--- src/config/site.js
+++ src/config/site.js
@@ -8,6 +8,7 @@ export const siteConfig = {
   description:
     "An open design project articulating capture-resistant principles for democratic governance adequate to the 21st century.",
   defaultImage: "/redemocratize-og.jpg",
+  repo: "https://github.com/wryfi/redemocratize",
   license: {
     name: "CC BY-SA 4.0",
     url: "https://creativecommons.org/licenses/by-sa/4.0/",
